50fc069ba6ae871dfd8802f83ce34c1354b8527a
Aurelien-Be <abeveraggi@netcourrier.com>
2021-10-26 11:38:08 +0300

    maj

--- blog/views.py
+++ blog/views.py
@@ -27,5 +27,7 @@ def posts(request):
 def post_detail(request, slug):
     identified_post = get_object_or_404(Post, slug=slug)
     return render(request, "blog/post-detail.html", {
-             "post": identified_post
+             "post": identified_post,
+             #to have a list that is usefull on our template
+             "post_tags": identified_post.tags.all()
     } ) 
