ac37513e19b774412f3c051b54606bc226e90cf2
Aurelien-Be <abeveraggi@netcourrier.com>
2021-12-13 18:36:21 +0300

    Comment form update

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render, get_object_or_404
-from django.views.generic import ListView, DetailView
+from django.http import HttpResponseRedirect
+from django.urls import reverse #allows to construct urls automatically by just using the names of the urls 
+from django.views.generic import ListView
+from django.views import View 
 
 from .models import Post
 from .forms import CommentForm
@@ -56,13 +59,32 @@ class PostsView(ListView):
 #             "post_tags": identified_post.tags.all()
 #    } ) 
 #in class based view:
-class PostDetailView(DetailView):
-    template_name =  "blog/post-detail.html"   
-    model = Post
-    #this class view is able to search by a slug
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context["post_tags"] = self.object.tags.all()
-        #then we add the comment feature
-        context["comment_form"] = CommentForm() #then we can call the form in the template
-        return context
+class PostDetailView(View):
+
+    def get(self, request, slug):
+        post = Post.objects.get(slug=slug)
+        context = {
+            "post":post,
+            "post_tags": post.tags.all(),
+            "comment_form": CommentForm()
+        }
+        return render(request, "blog/post-detail.html", context )
+    
+    def post(self, request, slug):
+        comment_form = CommentForm(request.Post)
+        post = Post.objects.get(slug=slug) #we fetch the post
+
+        if comment_form.is_valid():
+            comment = comment_form.save(commit=False) #we can call save because its come from Forms, it is connected to the database
+            #commit=False create a new object and don't hit the database. In the CommentForm, we exclude the Post field, so it would cause pb without it 
+            comment.post = post #set the postfield to the post wich the commment should be related
+            comment.save()
+
+            return HttpResponseRedirect(reverse('post-detail-page', args=[slug])) 
+         
+        context = {
+            "post":post,
+            "post_tags": post.tags.all(),
+            "comment_form": CommentForm()
+        }
+        return render(request, "blog/post-detail.html", context )
