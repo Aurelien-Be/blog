c38be5358167e7110e77da75e2d004c6cd95137b
Aurelien-Be <abeveraggi@netcourrier.com>
2022-01-22 15:01:26 +0300

    update read later style

--- blog/views.py
+++ blog/views.py
@@ -110,6 +110,22 @@ class SearchView(ListView):
     
 
 class ReadLaterView(View):
+
+    def get(self, request):
+        stored_posts = request.session.get('stored_posts')
+        
+        context = {}
+
+        if stored_posts is None or len(stored_posts) == 0:
+            context["posts"] = []
+            context["has_posts"] = False
+        else:
+            posts = Post.objects.filter(id__in=stored_posts)
+            context["posts"]= posts
+            context["has_posts"] = True
+
+        return render(request, "blog/stored-posts.html", context)
+
     def post(self, request):
         stored_posts = request.session.get('stored_posts') #.get to not have an error if there is no stored posts
 
@@ -120,6 +136,7 @@ class ReadLaterView(View):
         
         if post_id not in stored_posts:
           stored_posts.append(post_id)
+          request.session["stored_posts"] = stored_posts
         
         return HttpResponseRedirect('/')
 
